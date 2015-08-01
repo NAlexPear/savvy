fa16d674e3f98b4be31e883ff1d1ce249108f2c8
Alex Pearson <pearsonn.alex@gmail.com>
2015-08-01 04:01:02 +0000

    Start the curriculum bubbles

--- theme/js/wordfades.js
+++ theme/js/wordfades.js
@@ -17,6 +17,20 @@ $(function() {
             }
             
         }); 
+        //repeat for paragraph text
+        $('p').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1000);
+                    
+            }
+            
+        }); 
     
     });
     
