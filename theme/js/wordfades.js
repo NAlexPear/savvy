a9acf377ee187f3da4227c03862258af7b75df0e
Alex Pearson <alex@alexpear.com>
2015-08-01 09:57:59 -0500

    Pull from origin merge

--- theme/js/wordfades.js
+++ theme/js/wordfades.js
@@ -17,8 +17,22 @@ $(function() {
             }
             
         }); 
-        //repeat for paragraph text
-        $('p').each( function(i){
+        //repeat for "how it works" section
+        $('#howworks').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1800);
+                    
+            }
+            
+        });
+         //repeat for "what do you learn" section
+        $('#whatlearn').each( function(i){
             
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
