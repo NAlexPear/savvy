2cebe814e11fa09d74e59212fd97a6dcbf52d565
Alex Pearson <alex@alexpear.com>
2015-08-06 14:32:45 -0500

    Implement contact form

---
+++ theme/js/contact.js
@@ -0,0 +1,6 @@
+$(function(d, t){
+   var g = d.createElement(t),
+       s = d.getElementsByTagName(t)[0];
+   g.src = "http://www.foxyform.com/js.php?id=693137&sec_hash=e344ed818bd&width=350px";
+   s.parentNode.insertBefore(g, s);
+}(document, "script"));
