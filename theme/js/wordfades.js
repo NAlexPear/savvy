777baeef157ad97876b32e23ca81751b4be5dc6f
Alex Pearson <alex@alexpear.com>
2015-08-20 11:51:19 -0500

    Begin adding sign-up button (non-working)

--- theme/js/wordfades.js
+++ theme/js/wordfades.js
@@ -4,19 +4,19 @@ $(function() {
     $(window).scroll( function(){
 
         /* Check the location of each desired element */
-        $('h2').each( function(i){
-
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},300);
-
-            }
-
-        });
+        // $('h2').each( function(i){
+        //
+        //     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        //     var bottom_of_window = $(window).scrollTop() + $(window).height();
+        //
+        //     /* If the object is completely visible in the window, fade it it */
+        //     if( bottom_of_window > bottom_of_object ){
+        //
+        //         $(this).animate({'opacity':'1'},300);
+        //
+        //     }
+        //
+        // });
         //repeat for "how it works" section
         $('#howworks').each( function(i){
 
