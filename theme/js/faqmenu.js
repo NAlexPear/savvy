fb8d9dce66d6881fbb93503f401d1a6c32b2ca3b
Alex Pearson <alex@alexpear.com>
2015-08-25 15:23:07 -0500

    First JQuery implementation of FAQ page

--- theme/js/faqmenu.js
+++ theme/js/faqmenu.js
@@ -1,5 +1,50 @@
 $(function(){
   $('#faq-list h3:first-child').click(function(){
-    $('.response').addClass('active-response');
+    var oldAns = $('.active-response');
+    var newAns = $('#faq1');
+    oldAns.removeClass('active-response');
+    newAns.addClass('active-response');
+  });
+  $('#faq-list h3:second-child').click(function(){
+    var oldAns = $('.active-response');
+    var newAns = $('#faq2');
+    oldAns.removeClass('active-response');
+    newAns.addClass('active-response');
+  });
+  $('#faq-list h3:third-child').click(function(){
+    var oldAns = $('.active-response');
+    var newAns = $('#faq3');
+    oldAns.removeClass('active-response');
+    newAns.addClass('active-response');
+  });
+  $('#faq-list h3:fourth-child').click(function(){
+    var oldAns = $('.active-response');
+    var newAns = $('#faq4');
+    oldAns.removeClass('active-response');
+    newAns.addClass('active-response');
+  });
+  $('#faq-list h3:fifth-child').click(function(){
+    var oldAns = $('.active-response');
+    var newAns = $('#faq5');
+    oldAns.removeClass('active-response');
+    newAns.addClass('active-response');
+  });
+  $('#faq-list h3:sixth-child').click(function(){
+    var oldAns = $('.active-response');
+    var newAns = $('#faq6');
+    oldAns.removeClass('active-response');
+    newAns.addClass('active-response');
+  });
+  $('#faq-list h3:seventh-child').click(function(){
+    var oldAns = $('.active-response');
+    var newAns = $('#faq7');
+    oldAns.removeClass('active-response');
+    newAns.addClass('active-response');
+  });
+  $('#faq-list h3:eighth-child').click(function(){
+    var oldAns = $('.active-response');
+    var newAns = $('#faq8');
+    oldAns.removeClass('active-response');
+    newAns.addClass('active-response');
   });
 });
