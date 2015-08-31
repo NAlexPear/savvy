afc04c0106acc9ae34f18b98bfeb315756b6df51
Alex Pearson <alex@alexpear.com>
2015-08-31 13:04:11 -0500

    Successfully implement mmenu.js

--- theme/js/mmenu.js
+++ theme/js/mmenu.js
@@ -1,11 +1,26 @@
 $(function(){
-    $('.menu-icon').click(function(){
+  $('.menu-icon').click(function(){
+    if($('#menubar').hasClass('expanded')){
+        $('body').animate({left:'0px'});
+        $('#menubar').removeClass('expanded').animate({left:'-800px'});
+    } else {
         $('body').animate({left:'200px'});
-        $('#menubar').animate({left:'0px'});
+        $('#menubar').addClass('expanded').animate({left:'0px'});
+      }
     });
 });
 
-// function(){
-//     $('body').animate({left:'0px'});
-//     $('#menubar').animate({left:'-800px'});
+// USE 'ONE'
+// $(function handler1(){
+//   $('body').animate({left:'200px'});
+//   $('#menubar').animate({left:'0px'});
+//   $('.menu-icon').one("click", handler2);
 // });
+//
+// $(function handler2(){
+//   $('body').animate({left:'0px'});
+//   $('#menubar').animate({left:'-800px'});
+//   $('.menu-icon').one('click', handler1);
+// });
+//
+// $('.menu-icon').one('click', handler1);
