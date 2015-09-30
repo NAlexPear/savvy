4bcda6a81507e348c9c7c286d97e0eadeb7e2e74
Alex Pearson <pearsonn.alex@gmail.com>
2015-09-30 21:18:58 +0000

    Finish Wk3Mod3

---
+++ class-slides/class-materials/week-3/module-3/arrays/array2.js
@@ -0,0 +1,13 @@
+var greeting = "Hello and welcome to my webpage!"
+
+greeting[0]
+greeting[1]
+greeting[28]
+greeting[29]
+
+var index = 2
+greeting[index]
+
+var len = greeting.length
+greeting[len]
+greeting[len - 1]
