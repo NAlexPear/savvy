c8916d4285703c0532fb1f5e5ca916baa063dcec
Alex Pearson <pearsonn.alex@gmail.com>
2015-10-20 20:09:07 +0000

    Commit before beginning work on Wk4Mod2

--- class-slides/class-materials/week-4/module-1/ajax/ajax2.js
+++ class-slides/class-materials/week-4/module-1/ajax/ajax2.js
@@ -1,5 +1,5 @@
 var to_be_run_on_server_response = function(weather_data){
-  $('#container').append(weather_data.main.temp)
-}
+  $('#output').append(weather_data.main.temp)
+};
 
 $.get('http://api.openweathermap.org/data/2.5/weather?q=Nashville&appid=bd82977b86bf27fb59a04b61b657fb6f').success(to_be_run_on_server_response)
