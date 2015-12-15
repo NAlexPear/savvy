8cf5e71aefc98aade3a5a3ead72b239feb1527a6
Alex Pearson <pearsonn.alex@gmail.com>
2015-12-15 22:58:50 +0000

    Add devDependencies in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,17 @@
 (function(){
     "use strict";
     
+    //set up requires for gulp tasks
     var gulp = require("gulp");
+    var usemin = require('gulp-usemin');
+    var uglify = require('gulp-uglify');
+    var minifyHtml = require('gulp-minify-html');
+    var minifyCss = require('gulp-minify-css');
+    var imagemin = require('gulp-imagemin');
+    var pngquant = require('imagemin-pngquant');
+    var gzip = require('gulp-gzip');
+    var critical = require('critical');
+    var useref = require('gulp-useref');
+    var gulpif = require('gulp-if');
     
 }());
