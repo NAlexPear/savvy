677b9aab4808fd8202156f60ee85c07cf3d29f67
Alex Pearson <pearsonn.alex@gmail.com>
2015-12-15 23:01:31 +0000

    Move themify-icons to fonts folder, add port to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -14,4 +14,14 @@
     var useref = require('gulp-useref');
     var gulpif = require('gulp-if');
     
+    //Porters of non-critical content
+    gulp.task('font-port', function(){
+      gulp.src(['theme/fonts/**/*'])
+        .pipe(gulp.dest('public/theme/fonts'));
+    });
+    gulp.task('other-image-port', function () {
+        gulp.src(['theme/images/**/*.svg','theme/images/**/*.ico'])
+            .pipe(gulp.dest('public/theme/images'));
+    });
+    
 }());
