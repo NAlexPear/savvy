99c24fe23d2bbeb38d6a4fe07683406fb7741ccc
Alex Pearson <pearsonn.alex@gmail.com>
2015-12-15 23:20:23 +0000

    Complete gulpfile.js (imagemin WIP)

--- gulpfile.js
+++ gulpfile.js
@@ -24,4 +24,50 @@
             .pipe(gulp.dest('public/theme/images'));
     });
     
+    //image minifier (no CSS, HTML, or JS)
+    // gulp.task('image-min', function () {
+    //     gulp.src(['theme/images/**/*.jpg','theme/images/**/*.png'])
+    //         .pipe(imagemin({
+    //           optimizationLevel: 7,
+    //           progressive:true,
+    //           use: [pngquant()]
+    //         }))
+    //         .pipe(gulp.dest('public/theme/images'));
+    // });
+    
+    //CSS and JS minifier, retaining async on javascript files, after all other files have been ported over
+    gulp.task('async',['font-port','other-image-port'],function(){
+      return gulp.src('index.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('public'));
+    });
+    
+    //CSS inliner post-CSS and JS minification, pre-HTML minification and gzipping
+    gulp.task('css-inline',['async'], function(){
+      return critical.generateInline({
+        base:'public/',
+        src:'index.html',
+        dest: 'public/index.html',
+        width: 1300,
+        height: 900
+      });
+    });
+    
+    //HTML minifier (run after ports, image-minification, and critical CSS inlining)
+    gulp.task('cruncher', ['css-inline'], function() {
+       gulp.src('public/index.html')
+        .pipe(usemin({
+            assetsDir: '',
+            html: [minifyHtml({empty:true})]
+        }))
+        .pipe(gulp.dest('public'));
+    });
+    
+    //default task (gzips final files)
+    gulp.task('default',['cruncher'],function () {
+      gulp.src(['public/**/*','!public/**/*.gz','!public/**/*.md','!public/**/*.txt', '!public/**/*.json','!public/**/*.xml', '!public/theme/images/**/*'])
+        .pipe(gzip()).pipe(gulp.dest('public'));
+    });
 }());
