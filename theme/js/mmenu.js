97b89b3111eaeb95404942095678c9e93f9900a9
Alex Pearson <alex@alexpear.com>
2015-12-19 14:38:47 -0600

    Add functioning drop-down menu (auto-populate WIP)

--- theme/js/mmenu.js
+++ theme/js/mmenu.js
@@ -1,18 +1,74 @@
 $(function(){
-  $('.menu-icon').click(function(){
-    if($('#menubar').hasClass('expanded')){
-        $('body').animate({left:'0px'});
-        $('#menubar').removeClass('expanded').animate({left:'-250px'});
+  //set up function to change MORE to LESS on different events
+  var more_less = function () {
+    //include arrow-turning function
+    arrow();
+    //set text changes for MORE-LESS
+    var $target = $('.overflow>div>span.more-less');
+    var text = $target.text();
+    if(text === 'MORE'){
+      $target.text('LESS');
     } else {
-        $('body').animate({left:'200px'});
-        $('#menubar').addClass('expanded').animate({left:'-25px'});
-      }
-    });
-  $('#menubar a').click(function(){
-    if($('#menubar').hasClass('expanded')){
-      $('body').animate({left:'0px'});
-      $('#menubar').removeClass('expanded').animate({left:'-250px'});
+      $target.text('MORE');
     }
-  });
-});
+  };
 
+  //rotate arrow using CSS transforms
+  var arrow = function () {
+    var $target = $('.overflow>div');
+    if($target.hasClass('turnt')){
+      $target.removeClass('turnt');
+      console.log('now we aint');
+    } else {
+      $target.addClass('turnt');
+      console.log('we turnt!');
+    }
+  };
+
+  //only let the 'more' option appear when the window is less than 1000px wide
+  if($(window).width() < 1000 && $('.overflow').hasClass('hidden')){
+    //remove 'hidden' class
+    $('.overflow').removeClass('hidden');
+
+    //set dropdown behaviors for different screen widths
+      //mobile widths (<640px)
+      if($(window).width() < 640){
+        //set mobile dropdown target
+        var drop = $('.dropdown');
+
+        //bring dropdown menu in from right on touch (mobile only)
+        $('.overflow').on('click',function(){
+          more_less();
+          if(drop.hasClass('active')){
+            drop.animate({
+              'right':'-100px',
+              'opacity':'0'
+            },300).removeClass('active');
+          } else {
+            drop.animate({
+              'right':'0',
+              'opacity':'1'
+            },300).addClass('active');
+          }
+        });
+      } else {
+        //set desktop dropdown target
+        var drop = $('.dropdown');
+        //slide menu down from top on hover (desktop only)
+        $('.overflow').on('click',function(){
+          more_less();
+          if(drop.hasClass('active')){
+            drop.animate({
+              'opacity':'0',
+              'top':'-500px'
+            },500).removeClass('active');
+          } else {
+            drop.animate({
+              'opacity':'1',
+              'top':'32px'
+            },500).addClass('active');
+          }
+        });
+      }
+  }
+});
